12cafa722f42617a1478d012678e2e2cd61a935e
Darryl Subal <dsubal@att.net>
2023-04-22 10:55:17 -0400

    category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,67 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoriesData) => {
+    res.json(categoriesData);
+  });
 });
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoriesData) => {
+    res.json(categoriesData);
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  try {
+    const categoriesData = Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(Category.category_name);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name,
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
